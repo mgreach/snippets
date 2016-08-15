e1914bdb9aa0347216d787f273a9e75f71b2ca69
Igor <zimzigor@gmail.com>
2016-08-15 17:29:44 +0300

    кавычки

--- main/views.py
+++ main/views.py
@@ -64,4 +64,5 @@ def snippets(keyword):
 def index(request):
     keyword = request.GET.get('key', '')
     context = snippets(keyword)
-    return HttpResponse(json.dumps(context).decode('unicode-escape').encode('utf8'))
+    #return HttpResponse(json.dumps(context).decode('unicode-escape').encode('utf8'))
+    return HttpResponse(json.dumps(context))
